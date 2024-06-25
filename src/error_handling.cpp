5971b007f02e2ec4fb656df863ec84ca4d15d794
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-06-25 08:51:46 +0200

    pdu only cuts sdc on error. Motherboard does all the decisions

--- src/error_handling.cpp
+++ src/error_handling.cpp
@@ -6,12 +6,12 @@ pdu_12v_command error_handling::approve(pdu_12v_command cmd) {
   if (canzero_get_error_any_short() == error_flag_ERROR) {
     // affected channel is already off anyway
     pdu12::set_sdc(false);
-    return pdu_12v_command_TELEMETRY;
+    return cmd;
   }
   if (canzero_get_error_heartbeat_miss() == error_flag_ERROR) {
     // with only telemetry on nothing too bad can happen right?
     pdu12::set_sdc(false);
-    return pdu_12v_command_TELEMETRY;
+    return cmd;
   }
 
   if (canzero_get_error_level_mcu_temperature() == error_level_ERROR) {
